accb3403557ff1fef8365aac52926f5179c2b006
makikooni <92479890+makikooni@users.noreply.github.com>
2022-04-08 23:31:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
 window.onload = function() {
-	let btn=
-	document.getElementById("jump");
 	let count = 0;
+	var t = Date.now();
+	var speed = 25;
 	
-	btn.onclick = function() {
-		count += 1;
-	}
-
-}
-
 var canvas =
 document.getElementById("canvas");
-context = canvas.getContext("2d");
+var context =
+ canvas.getContext("2d");
 var x = 300;
 var y = 350;
 
 context.arc(x, y, 50, 0, 2 * Math.PI);
-context.fillStyle="red";
+context.fillStyle="#ff99ff";
 context.fill();
+
+	
+	document.ontouchstart = function() {
+		count += 1;
+		y -= 25;}
+		
+	
+		function draw() {
+		
+		var timePassed = (Date.now() - t) /1000;
+		t = Date.now();
+		
+		if(y <= 350) {
+			speed += 7500 *timePassed;
+			y += speed*timePassed;
+		}
+		
+		if(y >350) {
+			count = 0;
+		}
+		
+		if(y < 0) {
+			count = 0;
+		}
+		
+		context.clearRect(0, 0, 600, 400);
+		speed = 25
+		
+		context.beginPath();
+		context.arc(x, y, 50, 0, 2 * Math.PI);
+		context.fillStyle="ff99ff";
+		context.fill();
+		
+		context.font = '25px Arial';
+		context.fillStyle = 'white';
+		context.fillText("Count:" + count, 20, 30);
+
+	
+	window.requestAnimationFrame(draw);
+}
+
+draw();
+}
